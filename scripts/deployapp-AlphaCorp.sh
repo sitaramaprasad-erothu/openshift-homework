038d7dda5cb6ba6186b47ba797284c39278e26fe
tripathisushil <tripathis_k@hotmail.com>
2018-11-21 15:48:19 -0500

    Update deployapp-AlphaCorp.sh

--- scripts/deployapp-AlphaCorp.sh
+++ scripts/deployapp-AlphaCorp.sh
@@ -3,6 +3,7 @@
 echo "Deploy App to Alpha Corp"
 cd /root/openshift-homework/container-pipelines/basic-spring-boot
 ansible-galaxy install -r requirements.yml --roles-path=galaxy
+
 ansible-playbook -i .applier/ galaxy/openshift-applier/playbooks/openshift-cluster-seed.yml 
 
 echo "Sleep 2 min"
