cd7b569f657f681cfee2a41e170048b5d34ef738
tripathisushil <tripathis_k@hotmail.com>
2018-11-20 23:33:31 -0500

    11202018-11:33PM
    11202018-11:33PM

--- scripts/generate_inventory.sh
+++ scripts/generate_inventory.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+echo "Copy a new htpasswd.openshift file to /root"
+cp -f /root/openshift-homework/htpasswd.openshift /root/htpasswd.openshift 
+
 echo "Copy a new hosts file to /etc/ansible/hosts"
-cp /root/ocp_advanced_deployment_homework/hosts /etc/ansible/hosts
+cp -f /root/ocp_advanced_deployment_homework/hosts /etc/ansible/hosts
 
 echo "Set the current GUID to generate the inventory"
 GUID=`hostname|awk -F. '{print $2}'`
