9cbcd9e56cd8fd3aa0166727624ccf18cfffe510
tripathisushil <tripathis_k@hotmail.com>
2018-11-25 12:35:02 -0500

    1125-12:35PM
    1125-12:35PM

---
+++ scripts/cicd-beta.sh
@@ -0,0 +1,17 @@
+# Create Projects
+oc new-project task-dev --display-name="Tasks - Dev"
+oc new-project task-test --display-name="Tasks - TEST"
+oc new-project task-prod --display-name="Tasks - Prod"
+oc new-project cicd --display-name="CI/CD"
+
+# Grant Jenkins Access to Projects
+oc policy add-role-to-group edit system:serviceaccounts:cicd -n task-dev
+oc policy add-role-to-group edit system:serviceaccounts:cicd -n task-test
+oc policy add-role-to-group edit system:serviceaccounts:cicd -n task-prod
+
+# Deploy Demo
+oc new-app -n cicd -f /root/openshift-homework/yaml/cicd-template.yaml
+
+# Sleep for 5 minutes and then Start Pipeline
+sleep 300
+oc start-build tasks-pipeline
