1b7475446dd3a60d213ebb9ab6d08477f434142c
tripathisushil <tripathis_k@hotmail.com>
2018-11-26 18:20:26 -0500

    6:20Pm

--- scripts/cicd.sh
+++ scripts/cicd.sh
@@ -6,7 +6,7 @@ oc new-project alpha-task-test --display-name="Alpha-Tasks-TEST"
 oc new-project alpha-task-prod --display-name="Alpha-Tasks-Prod"
 oc new-project alpha-task-build --display-name="Alpha-Tasks-Build"
 
-# Grant Jenkins Access to Projects
+## Grant Jenkins Access to Projects
 oc policy add-role-to-group admin system:serviceaccount:alpha-task-build:jenkins -n alpha-task-dev
 oc policy add-role-to-group admin system:serviceaccount:alpha-task-build:jenkins -n alpha-task-test
 oc policy add-role-to-group admin system:serviceaccount:alpha-task-build:jenkins -n alpha-task-prod
@@ -20,7 +20,7 @@ oc policy add-role-to-group admin alpha-corp -n alpha-task-build
 
 oc adm pod-network join-projects --to=alpha-task-build alpha-task-dev alpha-task-test alpha-task-prod >/dev/null 2>&1
 
-oc login -u amy -p r3dh4t1!
+# oc login -u amy -p r3dh4t1!
 
 oc project alpha-task-build
 
