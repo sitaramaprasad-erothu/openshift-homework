e25003a865d9a3c074939ed7d427b254c3dc8ccd
tripathisushil <tripathis_k@hotmail.com>
2018-11-26 14:47:52 -0500

    cicd

--- scripts/cicd.sh
+++ scripts/cicd.sh
@@ -1,20 +1,25 @@
 # Create Projects
-oc new-project task-dev --display-name="Tasks - Dev"
-oc new-project task-test --display-name="Tasks - TEST"
-oc new-project task-prod --display-name="Tasks - Prod"
-oc new-project cicd --display-name="CI/CD"
+comp=$1
+oc login -u system:admin
+oc new-project $comp-task-dev --display-name="Tasks - Dev"
+oc new-project $comp-task-test --display-name="Tasks - TEST"
+oc new-project $comp-task-prod --display-name="Tasks - Prod"
+oc new-project $comp-task-build --display-name="CI/CD"
 
 # Grant Jenkins Access to Projects
-oc policy add-role-to-group edit system:serviceaccounts:cicd -n task-dev
-oc policy add-role-to-group edit system:serviceaccounts:cicd -n task-test
-oc policy add-role-to-group edit system:serviceaccounts:cicd -n task-prod
+oc policy add-role-to-group edit system:serviceaccounts:task-build -n $comp-task-dev
+oc policy add-role-to-group edit system:serviceaccounts:task-build -n $comp-task-test
+oc policy add-role-to-group edit system:serviceaccounts:task-build -n $comp-task-prod
 
-oc adm pod-network join-projects --to=cicd task-dev task-test task-prod >/dev/null 2>&1
-oc new-app jenkins-persistent -n cicd
+oc adm pod-network join-projects --to=$comp-task-build $comp-task-dev $comp-task-test $comp-task-prod >/dev/null 2>&1
+
+oc login -u amy -p r3dh4t1!
+
+oc new-app jenkins-persistent -n $comp-task-build
 
 
 # Deploy Demo
-oc new-app -n cicd -f /root/openshift-homework/yaml/cicd-template.yaml
+oc new-app -n $comp-task-build -f /root/openshift-homework/yaml/cicd-template.yaml
 
 # Sleep for 5 minutes and then Start Pipeline
 sleep 300
