1399979ab8d9a0bf163e5e737775d7c6f8fe8e16
tripathisushil <tripathis_k@hotmail.com>
2018-11-26 21:36:50 -0500

    9;36

--- scripts/cicd.sh
+++ scripts/cicd.sh
@@ -4,19 +4,19 @@ oc login -u system:admin
 oc new-project alpha-task-dev --display-name="Alpha-Tasks-Dev"
 oc new-project alpha-task-test --display-name="Alpha-Tasks-TEST"
 oc new-project alpha-task-prod --display-name="Alpha-Tasks-Prod"
-oc new-project alpha-task-build --display-name="Alpha-Tasks-Build"
+oc new-project cicd --display-name="Alpha-Tasks-Build"
 
 ## Grant Jenkins Access to Projects
-oc policy add-role-to-group edit system:serviceaccount:alpha-task-build -n alpha-task-dev
-oc policy add-role-to-group edit system:serviceaccount:alpha-task-build -n alpha-task-test
-oc policy add-role-to-group edit system:serviceaccount:alpha-task-build -n alpha-task-prod
-oc policy add-role-to-group edit system:serviceaccount:alpha-task-build -n alpha-task-build
+oc policy add-role-to-group edit system:serviceaccount:cicd -n alpha-task-dev
+oc policy add-role-to-group edit system:serviceaccount:cicd -n alpha-task-test
+oc policy add-role-to-group edit system:serviceaccount:cicd -n alpha-task-prod
+oc policy add-role-to-group edit system:serviceaccount:cicd -n alpha-task-build
 
 
 oc policy add-role-to-group admin alpha-corp -n alpha-task-dev
 oc policy add-role-to-group admin alpha-corp -n alpha-task-test
 oc policy add-role-to-group admin alpha-corp -n alpha-task-prod
-oc policy add-role-to-group admin alpha-corp -n alpha-task-build
+oc policy add-role-to-group admin alpha-corp -n cicd
 
 oc adm pod-network join-projects --to=alpha-task-build alpha-task-dev alpha-task-test alpha-task-prod >/dev/null 2>&1
 
@@ -24,7 +24,7 @@ oc adm pod-network join-projects --to=alpha-task-build alpha-task-dev alpha-task
 
 oc project alpha-task-build
 
-#oc new-app jenkins-persistent -n alpha-task-build
+oc new-app jenkins-persistent -n cicd
 #oc new-app jenkins-ephemeral -n alpha-task-build
 
 # Deploy Demo
