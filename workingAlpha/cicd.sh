736b34241ecc324d874d6fb5cc23dd77d4382407
tripathisushil <tripathis_k@hotmail.com>
2018-11-27 16:45:53 -0500

    cicd

--- workingAlpha/cicd.sh
+++ workingAlpha/cicd.sh
@@ -1,13 +1,8 @@
 # Create Projects
-#oc new-project alpha-task-dev --display-name="Alpha-Tasks-Dev"
-#oc new-project alpha-task-test --display-name="Alpha-Tasks-TEST"
-#oc new-project alpha-task-prod --display-name="Alpha-Tasks-Prod"
-#oc new-project alpha-cicd-dev --display-name="Alpha-CICD-Task"
-
-#oc label namespace alpha-task-dev client=alpha
-#oc label namespace alpha-task-test client=alpha
-#oc label namespace alpha-task-prod client=alpha
-#oc label namespace alpha-cicd-dev client=alpha
+#oc new-project beta-task-dev --display-name="Beta-Tasks - Dev"
+#oc new-project beta-task-test --display-name="Beta-Tasks - TEST"
+#oc new-project beta-task-prod --display-name="Beta-Tasks - Prod"
+#oc new-project beta-cicd --display-name="Beta-CI/CD"
 
 oc adm new-project alpha-task-dev --node-selector='client=alpha'
 oc adm new-project alpha-task-test --node-selector='client=alpha'
@@ -16,11 +11,14 @@ oc adm new-project alpha-cicd-dev --node-selector='client=alpha'
 
 # Grant Jenkins Access to Projects
 oc policy add-role-to-group edit system:serviceaccounts:alpha-cicd-dev -n alpha-task-dev
+sleep 10
 oc policy add-role-to-group edit system:serviceaccounts:alpha-cicd-dev -n alpha-task-test
+sleep 10
 oc policy add-role-to-group edit system:serviceaccounts:alpha-cicd-dev -n alpha-task-prod
-
+sleep 10
+oc new-app jenkins-persistent
 # Deploy Demo
-oc new-app -n alpha-cicd-dev -f cicd_template.yaml
+oc new-app -n alpha-cicd-dev -f cicd-template.yaml
 
 # Sleep for 5 minutes and then Start Pipeline
 sleep 300
--- workingBeta/cicd.sh
+++ workingBeta/cicd.sh
@@ -2,13 +2,16 @@
 oc new-project beta-task-dev --display-name="Beta-Tasks - Dev"
 oc new-project beta-task-test --display-name="Beta-Tasks - TEST"
 oc new-project beta-task-prod --display-name="Beta-Tasks - Prod"
-oc new-project beta-cicd --display-name="CI/CD"
+oc new-project beta-cicd --display-name="Beta-CI/CD"
 
 # Grant Jenkins Access to Projects
 oc policy add-role-to-group edit system:serviceaccounts:beta-cicd -n beta-task-dev
+sleep 10
 oc policy add-role-to-group edit system:serviceaccounts:beta-cicd -n beta-task-test
+sleep 10
 oc policy add-role-to-group edit system:serviceaccounts:beta-cicd -n beta-task-prod
-
+sleep 10
+oc new-app jenkins-persistent
 # Deploy Demo
 oc new-app -n beta-cicd -f cicd-template.yaml
 
