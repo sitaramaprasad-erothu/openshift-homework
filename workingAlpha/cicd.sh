18e644c28b8cf28481018abd4ba7fb9a793a6e5c
tripathisushil <tripathis_k@hotmail.com>
2018-11-27 16:00:57 -0500

    CICD-Alpha

--- workingAlpha/cicd.sh
+++ workingAlpha/cicd.sh
@@ -1,13 +1,18 @@
 # Create Projects
-oc new-project alpha-task-dev --display-name="Alpha-Tasks-Dev"
-oc new-project alpha-task-test --display-name="Alpha-Tasks-TEST"
-oc new-project alpha-task-prod --display-name="Alpha-Tasks-Prod"
-oc new-project alpha-cicd-dev --display-name="Alpha-CICD-Task"
+#oc new-project alpha-task-dev --display-name="Alpha-Tasks-Dev"
+#oc new-project alpha-task-test --display-name="Alpha-Tasks-TEST"
+#oc new-project alpha-task-prod --display-name="Alpha-Tasks-Prod"
+#oc new-project alpha-cicd-dev --display-name="Alpha-CICD-Task"
 
-oc label namespace alpha-task-dev client=alpha
-oc label namespace alpha-task-test client=alpha
-oc label namespace alpha-task-prod client=alpha
-oc label namespace alpha-cicd-dev client=alpha
+#oc label namespace alpha-task-dev client=alpha
+#oc label namespace alpha-task-test client=alpha
+#oc label namespace alpha-task-prod client=alpha
+#oc label namespace alpha-cicd-dev client=alpha
+
+oc adm new-project alpha-task-dev --node-selector='client=alpha'
+oc adm new-project alpha-task-test --node-selector='client=alpha'
+oc adm new-project alpha-task-prod --node-selector='client=alpha'
+oc adm new-project alpha-cicd-dev --node-selector='client=alpha'
 
 # Grant Jenkins Access to Projects
 oc policy add-role-to-group edit system:serviceaccounts:alpha-cicd-dev -n alpha-task-dev
