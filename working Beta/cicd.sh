f83c70562fcf5d7998c23436ffacbe660daf1305
tripathisushil <tripathis_k@hotmail.com>
2018-11-27 12:35:33 -0500

    cicd

---
+++ working Beta/cicd.sh
@@ -0,0 +1,17 @@
+# Create Projects
+oc new-project beta-task-dev --display-name="Beta-Tasks - Dev"
+oc new-project beta-task-test --display-name="Beta-Tasks - TEST"
+oc new-project beta-task-prod --display-name="Beta-Tasks - Prod"
+oc new-project beta-cicd --display-name="CI/CD"
+
+# Grant Jenkins Access to Projects
+oc policy add-role-to-group edit system:serviceaccounts:cicd -n beta-task-dev
+oc policy add-role-to-group edit system:serviceaccounts:cicd -n beta-task-test
+oc policy add-role-to-group edit system:serviceaccounts:cicd -n beta-task-prod
+
+# Deploy Demo
+oc new-app -n beta-cicd -f cicd-template.yaml
+
+# Sleep for 5 minutes and then Start Pipeline
+sleep 300
+oc start-build tasks-pipeline
