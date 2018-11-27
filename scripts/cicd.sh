2ff85c559702244763168d1c88c5c80d968df5de
tripathisushil <tripathis_k@hotmail.com>
2018-11-26 21:40:35 -0500

    9:40PM

--- scripts/cicd.sh
+++ scripts/cicd.sh
@@ -10,7 +10,7 @@ oc new-project cicd --display-name="Alpha-Tasks-Build"
 oc policy add-role-to-group edit system:serviceaccount:cicd -n alpha-task-dev
 oc policy add-role-to-group edit system:serviceaccount:cicd -n alpha-task-test
 oc policy add-role-to-group edit system:serviceaccount:cicd -n alpha-task-prod
-oc policy add-role-to-group edit system:serviceaccount:cicd -n alpha-task-build
+oc policy add-role-to-group edit system:serviceaccount:cicd -n cicd
 
 
 oc policy add-role-to-group admin alpha-corp -n alpha-task-dev
@@ -18,7 +18,7 @@ oc policy add-role-to-group admin alpha-corp -n alpha-task-test
 oc policy add-role-to-group admin alpha-corp -n alpha-task-prod
 oc policy add-role-to-group admin alpha-corp -n cicd
 
-oc adm pod-network join-projects --to=alpha-task-build alpha-task-dev alpha-task-test alpha-task-prod >/dev/null 2>&1
+oc adm pod-network join-projects --to=cicd alpha-task-dev alpha-task-test alpha-task-prod >/dev/null 2>&1
 
 # oc login -u amy -p r3dh4t1!
 
@@ -28,7 +28,7 @@ oc new-app jenkins-persistent -n cicd
 #oc new-app jenkins-ephemeral -n alpha-task-build
 
 # Deploy Demo
-oc new-app -n alpha-task-build -f /root/openshift-homework/yaml/cicd-template.yaml
+oc new-app -n cicd -f /root/openshift-homework/yaml/cicd-template.yaml
 
 # Sleep for 5 minutes and then Start Pipeline
 sleep 298
