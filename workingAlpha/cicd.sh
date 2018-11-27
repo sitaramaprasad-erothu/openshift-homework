ef3f96923b88121b06d7890c1d399fc8cfcd5554
tripathisushil <tripathis_k@hotmail.com>
2018-11-27 14:46:27 -0500

    cicd

---
+++ workingAlpha/cicd.sh
@@ -0,0 +1,17 @@
+# Create Projects
+oc new-project alpha-task-dev --display-name="Alpha-Tasks-Dev"
+oc new-project alpha-task-test --display-name="Alpha-Tasks-TEST"
+oc new-project alpha-task-prod --display-name="Alpha-Tasks-Prod"
+oc new-project alpha-cicd-dev --display-name="Alpha-CICD-Task"
+
+# Grant Jenkins Access to Projects
+oc policy add-role-to-group edit system:serviceaccounts:alpha-cicd-dev -n alpha-task-dev
+oc policy add-role-to-group edit system:serviceaccounts:alpha-cicd-dev -n alpha-task-test
+oc policy add-role-to-group edit system:serviceaccounts:alpha-cicd-dev -n alpha-task-prod
+
+# Deploy Demo
+oc new-app -n alpha-cicd-dev -f cicd-template.yaml
+
+# Sleep for 5 minutes and then Start Pipeline
+sleep 300
+oc start-build tasks-pipeline
