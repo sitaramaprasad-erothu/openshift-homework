12a0772ff812d014e405ecaea510db0c5c41ced0
tripathisushil <tripathis_k@hotmail.com>
2018-11-30 17:31:43 -0500

    cicd-alpha

--- scripts/alpha-corp-cicd.sh
+++ scripts/alpha-corp-cicd.sh
@@ -2,10 +2,12 @@
 
 oc login -u system:admin
 
+oc adm new-project alpha-cicd-dev --node-selector='client=alpha'
+oc adm new-project alpha-task-build --node-selector='client=alpha'
 oc adm new-project alpha-task-dev --node-selector='client=alpha'
 oc adm new-project alpha-task-test --node-selector='client=alpha'
 oc adm new-project alpha-task-prod --node-selector='client=alpha'
-oc adm new-project alpha-cicd-dev --node-selector='client=alpha'
+
 
 #oc create -f /root/openshift-homework/yaml/projectlimit.yaml -n alpha-task-dev
 #oc create -f /root/openshift-homework/yaml/projectlimit.yaml -n alpha-task-test
@@ -14,13 +16,12 @@ oc adm new-project alpha-cicd-dev --node-selector='client=alpha'
 
 
 # Grant Jenkins Access to Projects
+oc policy add-role-to-group edit system:serviceaccounts:alpha-cicd-dev -n alpha-task-build
 oc policy add-role-to-group edit system:serviceaccounts:alpha-cicd-dev -n alpha-task-dev
-sleep 10
 oc policy add-role-to-group edit system:serviceaccounts:alpha-cicd-dev -n alpha-task-test
-sleep 10
 oc policy add-role-to-group edit system:serviceaccounts:alpha-cicd-dev -n alpha-task-prod
-sleep 10
 
+oc adm policy add-role-to-group admin alpha-corp -n alpha-task-build
 oc adm policy add-role-to-group admin alpha-corp -n alpha-task-dev
 oc adm policy add-role-to-group admin alpha-corp -n alpha-task-test
 oc adm policy add-role-to-group admin alpha-corp -n alpha-task-prod
@@ -32,7 +33,7 @@ oc project alpha-cicd-dev
 
 oc new-app jenkins-persistent
 # Deploy Demo
-oc new-app -n alpha-cicd-dev -f /root/openshift-homework/yaml/alpha-corp-cicd-template.yaml
+oc new-app -n alpha-task-build -f /root/openshift-homework/yaml/alpha-corp-cicd-template.yaml
 
 # Sleep for 5 minutes and then Start Pipeline
 sleep 300
