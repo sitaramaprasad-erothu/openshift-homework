9f4ec7fd21f0970c7de55faf19779fa4e8cf3725
tripathisushil <tripathis_k@hotmail.com>
2018-11-30 13:53:46 -0500

    hosts-unintsall-defauly-namespace

--- scripts/uninstall_openshift.sh
+++ scripts/uninstall_openshift.sh
@@ -12,7 +12,21 @@ ansible all -a "rm -rf /root/.ansible/tmp/*"
 #Remove leftover content
 ansible all -a "rm -rf /home/ec2-user/.ansible/*"
 
+#Remove leftover content
+ansible all -a "rm -rf /root/tmp/*"
+
+#Remove leftover content
+ansible all -a "rm -rf /root/.ansible/tmp/*"
+
+#Remove leftover content
+ansible all -a "rm -rf /tmp/*"
+
 #Remove any data from the NFS Server
 ansible nfs -a "rm -rf /srv/nfs/*"
 
-rm -rf /etc/origin
+rm -rf /root/.ansible/tmp/*
+rm -rf /root/tmp/*
+rm -rf /home/ec2-user/.ansible/*
+rm -rf /root/.ansible/tmp/*
+rm -rf /etc/origin/*
+rm -rf /tmp/*
